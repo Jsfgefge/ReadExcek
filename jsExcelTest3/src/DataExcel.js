ebd39b585db6e57e4e4ad6524c0fab77021619c7
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2023-11-28 17:28:46 -0600

    Add files

---
+++ jsExcelTest3/src/DataExcel.js
@@ -0,0 +1,48 @@
+import * as Excel from 'exceljs'
+// var Excel = require('exceljs');
+
+const workbook = new Excel.default.Workbook();
+workbook.creator ="Naveen"; 
+workbook.modified ="Kumar";
+
+let data = [];
+var currRowArr;
+var currRow;
+
+async function test(){
+    await workbook.xlsx.readFile("Orders27112023.xlsx").then(function(){
+        var workSheet =  workbook.getWorksheet("Sheet1"); 
+    
+        
+    
+        workSheet.eachRow({ includeEmpty: true }, function(row, rowNumber) {
+            currRowArr = workSheet.getRow(rowNumber).values;
+    
+            currRow = workSheet.getRow(rowNumber); 
+            // console.log("User Name :" + currRow.getCell(1).value +", Password :" +currRow.getCell(2).value);
+            // console.log("User Name :" + row.values[1] +", Password :" +  row.values[2] ); 
+            
+    
+            // temp = currRowArr[2];
+    
+            data.push(currRowArr);
+            console.log(currRowArr);
+    
+            //  assert.equal(currRow.getCell(2).type, Excel.ValueType.Number); 
+           //  console.log("Row " + rowNumber + " = " + JSON.stringify(row.values));
+        //    console.log(Object.values(currRowArr));
+        //    console.log(currRowArr[2].hyperlink);
+        //    console.log(currRowArr[20].hyperlink);
+          });
+    });
+    
+    getData();
+}
+
+async function getData(){
+
+    await console.log(data);
+}
+
+
+test();
